357794a11a884126154b7b207ee2cb7df0ba1f6d
Selorm <ranselorm@outlook.com>
2024-11-03 19:55:41 +0000

    Responsive hero

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Icon } from "@iconify/react/dist/iconify.js";
 
 interface ButtonProps {
   text: string;
   icon?: string;
+  path?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ text, icon }) => {
+const Button: React.FC<ButtonProps> = ({ text, icon, path }) => {
   return (
-    <button className="bg-primary text-white text-xs px-6 py-3 rounded-full uppercase border-[1px] border-white mt-5 inline-flex items-center gap-x-2 hover:bg-accent transition duration-150">
-      {text}
-      {icon && <Icon icon={icon} />}
-    </button>
+    <Link to={`${path}`}>
+      <button className="bg-primary text-white text-xs px-6 py-4 rounded-full uppercase border-[1px] border-white mt-5 inline-flex items-center gap-x-2 hover:bg-accent transition duration-150 w-max">
+        {text}
+        {icon && <Icon icon={icon} />}
+      </button>
+    </Link>
   );
 };
 
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,3 @@
-// src/components/Hero.tsx
 import React from "react";
 import Button from "./Button";
 
@@ -28,10 +27,8 @@ const Hero: React.FC<HeroProps> = ({
         }}
       >
         <div className="absolute inset-0 bg-black opacity-50"></div>
-        {/* Overlay */}
       </div>
 
-      {/* Hero Content */}
       <div className="absolute inset-0 flex items-center justify-start px-8 lg:px-24 text-left text-white">
         <div className="w-full">
           <h1
@@ -47,10 +44,10 @@ const Hero: React.FC<HeroProps> = ({
             <p className="text-base leading-relaxed mt-12">{description}</p>
           )}
           {button && (
-            <div className="space-x-4 flex items-center">
-              <Button text="Get to know us" />
+            <div className="md:space-x-4 flex md:items-center flex-col md:flex-row md:mt-10">
+              <Button text="Get to know us" path="/about" />
               <Button
-                text="Coporate presentation"
+                text="Corporate presentation"
                 icon="material-symbols-light:download-sharp"
               />
             </div>
